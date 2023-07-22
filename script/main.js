aa2cb95febc6090f09c05eb293880e8335a0f5bb
606 <fgilliant01@gmail.com>
2023-07-22 11:00:19 +0900

    first commit

---
+++ script/main.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.open').addEventListener('click', toggleHamburger)
+
+function toggleHamburger() {
+    document.getElementsByClassName('nav').classList.toggle('open');
+    document.getElementsByClassName('menu').classList.toggle('open');
+
+}
